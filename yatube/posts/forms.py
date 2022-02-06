b3cbf799c76f8c046a07c46b5c7342b7c06d0e4a
Максим Шульгин <temulentum@gmail.com>
2022-02-06 15:54:56 +0300

    First try

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -4,15 +4,14 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+
+    def clean_text(self):
+        data = self.cleaned_data['text']
+
+        if self.cleaned_data['text'] == '':
+            raise forms.ValidationError('Пост не может быть пустым!')
+        return data
+
     class Meta:
         model = Post
-
         fields = ('text', 'group')
-
-        def clean_text(self):
-            data = self.cleaned_data['text']
-
-            if self.cleaned_data['text'] == '':
-                raise forms.ValidationError('Пост не может быть пустым!')
-
-            return data
